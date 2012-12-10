18ed5727ae2dbca5cf6e033f9937e2258fb12bd5
Brandon Vaughan <brandon.vaughan@nclud.com>
2012-12-10 08:50:16 -0500

    initialized testing files

---
+++ tests/example.js
@@ -0,0 +1,14 @@
+define( 'build/app/example', function( Example ) {
+
+  suite('example', function() {
+
+    test('Example exists', function() {
+    
+      var testExample = new Example();
+      asert(textExample, 'Example created';
+    
+    });
+
+  });
+
+});
